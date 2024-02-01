3eb6468a3f9c803088dca40bacc731dda63a0fbe
samneat <samjneat@gmail.com>
2024-02-01 09:10:16 +0000

    write code to pass tests

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -1,2 +1,8 @@
 def make_snippet(str):
-    return str
+    list_of_words = str.split(' ')
+    if len(list_of_words) > 5:
+        first_five = list_of_words[:5]
+        snippet = ' '.join(first_five)
+        return snippet + '...'
+    else:
+        return str
--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -13,3 +13,6 @@ def test_string_of_5_words_returns_string():
     result = make_snippet('one two three four five')
     assert result == 'one two three four five'
 
+def test_more_than_5_words_returns_string_and_elipsis():
+    result = make_snippet('one two three four five six')
+    assert result == 'one two three four five...'
